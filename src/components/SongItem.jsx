2f25a7d0e5cc09862416ecfb55ebb2fb5125caae
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2025-02-21 16:47:24 +0800

    i have change things

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -9,7 +9,8 @@ const SongItem = ({image, name, desc, id}) => {
     <div onClick={() => {
       playWithId(id)
 
-    }} className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
+    }} 
+      className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
       <img className="rounded" src={image} alt=""/>
       <p className="font-bold mt-2 mb-1">{name}</p>    
       <p className="text-slate-200 text-sm">{desc}</p>
