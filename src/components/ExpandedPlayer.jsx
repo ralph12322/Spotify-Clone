0d590f7c25bd29713a2411f1dbf8483aefcea029
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2025-02-27 00:22:27 +0800

    d

--- src/components/ExpandedPlayer.jsx
+++ src/components/ExpandedPlayer.jsx
@@ -1,13 +1,15 @@
 import React, { useContext } from 'react'
 import { PlayerContext } from '../context/PlayerContext'
 import Player from './Player'
+import Navbar from './Navbar';
 
 const ExpandedPlayer = () => {
 
   const {track} = useContext(PlayerContext);
   return (
     <div>
-      <img className='w-12' src={track.image} alt=''/>
+      <Navbar/>
+      <img className='w-full h-full' src={track.image} alt=''/>
       <Player/>
     </div>
   )
