798375f6308cd4c29ecd1549411752eaeb815aa9
ralph12322 <154263335+ralph12322@users.noreply.github.com>
2025-02-26 16:14:17 +0800

    Update SongItem.jsx

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -11,7 +11,7 @@ const SongItem = ({image, name, desc, id}) => {
 
     }} 
       className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
-      <img className="rounded h-[150px]" src={image} alt=""/>
+      <img className="rounded w-[150px] h-[150px]" src={image} alt=""/>
       <p className="font-bold mt-2 mb-1">{name}</p>    
       <p className="text-slate-200 text-sm">{desc}</p>
     </div>
