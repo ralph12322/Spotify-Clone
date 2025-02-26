7eb92e00e2afd8f69b5e229ca51220c9d3ccb055
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2025-02-26 19:57:59 +0800

    f

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,12 +6,12 @@ import { BrowserRouter } from 'react-router-dom'
 import PlayerContextProvider from './context/PlayerContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
     <BrowserRouter>
     <PlayerContextProvider>
      <App />
     </PlayerContextProvider>
 
     </BrowserRouter>
-  </StrictMode>,
+
 )
