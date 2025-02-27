d6d4be9f76663c95215895993bb5775053467f7e
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2025-02-27 09:42:08 +0800

    d

--- src/components/ExpandedPlayer.jsx
+++ src/components/ExpandedPlayer.jsx
@@ -7,7 +7,7 @@ const ExpandedPlayer = () => {
 
   const {track} = useContext(PlayerContext);
   return (
-    <div className='h-[100%] overflow-auto'>
+    <div className='h-[100%] overflow-auto rounded'>
       <Navbar/>
       <img className='w-full h-[77vh] object-contain rounded p-5' src={track.image} alt=''/>
       <Player/>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -9,7 +9,7 @@ const Player = () => {
   const navigate = useNavigate();
 
   return track ? (
-    <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
+    <div className='h-[10%] bg-black flex justify-between items-center text-white px-4 rounded'>
       <div className='hidden lg:flex items-center gap-4'>
         <img className='w-12' src={track.image} alt=''/>
         <div>
