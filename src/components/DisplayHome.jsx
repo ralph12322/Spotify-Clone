498e23398fd413e7e18d4a8cf615aec123ae877d
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2025-02-27 11:52:23 +0800

    grid added

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -16,7 +16,7 @@ const DisplayHome = () => {
       {albumData.map((item, index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item._id} image={item.image}/>))} 
       </div>
     </div>
-    <div className='mb-4'>
+    <div className='mb-4 grid grid-cols-6'>
       <h1 className='my-5 font-bold text-2xl'>Today's Biggest Hits</h1>
       <div className='flex overflow-auto'>
       {songData.map((item, index)=>(<SongItem key={index} name={item.name} desc={item.desc} id={item._id} image={item.image}/>))} 
