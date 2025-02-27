97bd67b9a5d509064974c97b2939a0d605589728
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2025-02-27 09:32:43 +0800

    full height for expanded player

--- src/components/ExpandedPlayer.jsx
+++ src/components/ExpandedPlayer.jsx
@@ -7,7 +7,7 @@ const ExpandedPlayer = () => {
 
   const {track} = useContext(PlayerContext);
   return (
-    <div className='h-[90%] overflow-auto'>
+    <div className='h-[100%] overflow-auto'>
       <Navbar/>
       <img className='w-full h-[80vh] object-contain rounded p-5' src={track.image} alt=''/>
       <Player/>
