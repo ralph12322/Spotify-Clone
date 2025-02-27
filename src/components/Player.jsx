28ee139220456f33d704b21746cdaa899ed9ea65
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2025-02-27 14:49:36 +0800

    d

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -10,7 +10,7 @@ const Player = () => {
 
   return track ? (
     <div className='h-[10%] bg-black flex justify-between items-center text-white px-4 rounded'>
-      <div className='hidden lg:flex items-center gap-4'>
+      <div className='hidden lg:flex items-center w-[15vw] gap-4'>
         <img className='w-12' src={track.image} alt=''/>
         <div>
           <p>{track.name}</p>
