1222f5e3bb50687ade24192003d2c4970851e69d
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2025-02-27 08:39:51 +0800

    mini again

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import { PlayerContext } from '../context/PlayerContext';
 
 const Navbar = () => {
 
   const navigate = useNavigate();
+  const { isMini, mini } = useContext(PlayerContext);
 
   return(
     <>
     <div className='w-full flex justify-between items-center font-semibold'>
       <div className='flex items-center gap-2'>
-        <img onClick={() => navigate(-1)} className='w-8 bg-black p-2 rounded-2xl cursor-pointer' src={assets.arrow_left} alt=''/>
+        <img onClick={() => {
+          navigate(-1);
+          if (mini) {
+            isMini();
+          }
+          }} className='w-8 bg-black p-2 rounded-2xl cursor-pointer' src={assets.arrow_left} alt=''/>
         <img onClick={() => navigate(+1)} className='w-8 bg-black p-2 rounded-2xl cursor-pointer' src={assets.arrow_right} alt=''/>
       </div>
       <div className='flex items-center gap-4'>
