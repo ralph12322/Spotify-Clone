a46c116e5ff09f058b2a0193da13c5716adf61e3
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2025-02-27 09:24:56 +0800

    d

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -14,7 +14,6 @@ const Player = () => {
         <img className='w-12' src={track.image} alt=''/>
         <div>
           <p>{track.name}</p>
-          <p>{track.desc.slice(0, 12)}</p>
         </div>
       </div>
       <div className='flex flex-col items-center gap-1 m-auto'>
