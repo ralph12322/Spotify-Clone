c700dc7e0498604ec17269213cf8ba3c50bb776a
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2025-02-27 14:45:05 +0800

    centered songs

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -10,11 +10,11 @@ const SongItem = ({image, name, desc, id}) => {
       playWithId(id)
 
     }} 
-      className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
-      <img className="rounded h-[125px]" src={image} alt=""/>
-      <p className="font-bold mt-2 mb-1">{name}</p>    
-      <p className="text-slate-200 text-sm">{desc}</p>
-    </div>
+    className="min-w-[180px] p-2 px-3 rounded cursor-pointer center hover:bg-[#ffffff26] flex flex-col items-center text-center">
+    <img className="rounded h-[125px]" src={image} alt=""/>
+    <p className="font-bold mt-2 mb-1">{name}</p>    
+    <p className="text-slate-200 text-sm">{desc}</p>
+  </div>
   )
 }
 
