ed445c978320671e98a1143ae3a3ea0d9fd5f2b8
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2025-02-28 22:22:45 +0800

    d

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -39,7 +39,7 @@ const DisplayAlbum = ({album}) => {
           </p>
         </div>
       </div>
-      <div className='pl-4 flex'>
+      <div className='p-4 flex'>
         <img className='cursor-pointer w-[5rem] h-[5rem]' onClick={playAlbum} src={assets.playAlbum} alt="" />
       </div>
       <div className='w-full grid grid-cols-3 sm:grid-cols-5 mt-10 mb-4 pl-2 text-[#a7a7a7]'>
