85adbf95ea9959357f4aa18b6de3727c706a3bb8
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2025-03-01 08:18:31 +0800

    rere

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -12,13 +12,13 @@ const DisplayHome = () => {
     <>
     <Navbar/>
     <div className='mb-4'>
-      <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
+      <h1 className='my-5 font-bold text-2xl'>Album Collection</h1>
       <div className='flex overflow-auto'>
       {albumData.map((item, index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item._id} image={item.image}/>))} 
       </div>
     </div>
     <div className='mb-4'>
-      <h1 className='my-5 font-bold text-2xl'>Today's Biggest Hits</h1>
+      <h1 className='my-5 font-bold text-2xl'>Song Collection</h1>
       <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 overflow-auto'>
       {songData.map((item, index)=>(<SongItem key={index} name={item.name} desc={item.desc} id={item._id} image={item.image}/>))} 
       </div>
